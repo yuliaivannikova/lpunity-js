a99e1705f301e55b42c6d7e58b656ce1a7a2f317
yuliaivannikova <yulia.godyk@gmail.com>
2019-11-08 09:25:10 +0200

    update

--- js/slider.js
+++ js/slider.js
@@ -1,33 +1,41 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  $(".single-item").slick();
 
+  $(".my-slider").slick({
+    centerMode: true,
+    centerPadding: "98px",
+    slidesToShow: 1,
+    autoplay: false,
+    autoplaySpeed: 1000,
+    arrows: true,
+    dots: true,
+    dotsClass: "slider__dots",
+    nextArrow:
+      '<button > <img src="./img/img12.png" alt="img" class="next"></button>',
+    prevArrow:
+      '<button > <img src="./img/img13.png" alt="img" class="prev"></button>',
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 
-    $('.single-item').slick();
-
-    $('.my-slider').slick({
-        centerMode: true,
-        centerPadding: '120px',
-        slidesToShow: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        useTransform: true,
-        responsive: [{
-                breakpoint: 768,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
+  //   $(".my-slider").css("border", "solid 1px red");
 });
